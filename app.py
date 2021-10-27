5c2a6a7fdbed65f5895dd12b61d94c1214655131
Divya-780 <divya@applines.com>
2021-10-27 10:18:24 +0530

    commit

--- app.py
+++ app.py
@@ -126,10 +126,7 @@ def logout():
 
 @app.after_request
 def add_header(response):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
+    
     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
     response.headers['Cache-Control'] = 'public, max-age=10'
     return response
